87ab5b359b7e7b261d85f44e494bfcc68c122ead
Alex Collins <alex.e.c@gmail.com>
2015-05-07 22:45:21 +0100

    File upload.

---
+++ src/main/java/swip/UploadServlet.java
@@ -0,0 +1,34 @@
+package swip;
+
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UploadServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        FileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        try {
+            upload
+                    .parseRequest(req)
+                    .stream()
+                    .filter(i -> !i.isFormField())
+                    .forEach(i -> System.out.printf("uploaded %s%n", i.getName()));
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+
+        resp.setStatus(302);
+        resp.addHeader("Location", "thank-you.html");
+    }
+}
