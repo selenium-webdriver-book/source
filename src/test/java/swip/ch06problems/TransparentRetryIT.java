b451a5b54979afae89a9b505008703cd9ceea81d
alex.collins <alex.e.c@gmail.com>
2015-06-24 22:47:41 +0100

    added two examples for chapter 06

---
+++ src/test/java/swip/ch06problems/TransparentRetryIT.java
@@ -0,0 +1,34 @@
+package swip.ch06problems;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import swip.ch07managingwebdriver.SeleniumWebDriverRunner;
+
+import javax.inject.Inject;
+
+@RunWith(SeleniumWebDriverRunner.class)
+public class TransparentRetryIT {
+    @Inject
+    private WebDriver driver;
+
+    @Test
+    public void transparentRetryExample() throws Exception {
+
+        driver.get("/login.html");
+
+        WebElement email = driver.findElement(By.name("email"));
+
+        driver.get("/login.html");
+
+        try {
+            email.sendKeys("foo@bar.com");
+        } catch (StaleElementReferenceException ignored) {
+            email = driver.findElement(By.name("email"));
+            email.sendKeys("foo@bar.com");
+        }
+    }
+}
