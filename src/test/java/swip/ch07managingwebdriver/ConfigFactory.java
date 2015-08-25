8c9572643377c3ab8ae4f565b1b6bbd4e06ecf31
alex.collins <alex.e.c@gmail.com>
2015-08-25 19:36:46 +0100

    Simplified configuration.

--- src/test/java/swip/ch07managingwebdriver/ConfigFactory.java
+++ src/test/java/swip/ch07managingwebdriver/ConfigFactory.java
@@ -1,12 +1,5 @@
 package swip.ch07managingwebdriver;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 public class ConfigFactory {
@@ -14,32 +7,8 @@ public class ConfigFactory {
     public static final String BASE_URL = baseUrl(LOGGER);
 
     private static String baseUrl(Logger logger) {
-        try {
-            String baseUrl = System.getProperty("webdriver.baseUrl", "http://" + getHostName() + ":8080");
-            logger.info("baseUrl " + baseUrl);
-            return baseUrl;
-        } catch (SocketException | UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static String getHostName() throws UnknownHostException, SocketException {
-        Optional<NetworkInterface> first = Collections
-                .list(NetworkInterface.getNetworkInterfaces())
-                .stream()
-                .peek(p -> LOGGER.info(p.getName() + " " + p.getInetAddresses().nextElement().getHostAddress()))
-                .filter(p -> Arrays.asList("docker0", "vboxnet1", "vboxnet0").contains(p.getName()))
-                .findFirst();
-        return first.isPresent()
-                ? first
-                .get()
-                .getInetAddresses()
-                .nextElement()
-                .getHostAddress()
-                : InetAddress.getLocalHost().getHostAddress();
-    }
-
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        System.out.println(getHostName());
+        String baseUrl = System.getProperty("webdriver.baseUrl", "http://localhost:8080");
+        logger.info("baseUrl " + baseUrl);
+        return baseUrl;
     }
 }
