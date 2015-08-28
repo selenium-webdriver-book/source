86b8b5bbf6e06ad69a9eaad04beb90d30a3b5f22
alex.collins <alex.e.c@gmail.com>
2015-08-28 09:17:21 +0100

    Removed unused files

--- bin/android.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-
-cd $(dirname $0)
-
-java -jar selendroid-standalone-0.15.0-with-dependencies.jar
--- bin/ios.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eux
-
-cd $(dirname $0)
-
-java -jar ios-server-0.6.5-jar-with-dependencies.jar -simulators
