48e1481253bbb92a7dcf4b706aed85b11b8f9651
alex.collins <alex.e.c@gmail.com>
2015-08-29 15:04:15 +0100

    Removed Java 8 code.

--- src/test/java/swip/ch02locatingelements/SimpleTable.java
+++ src/test/java/swip/ch02locatingelements/SimpleTable.java
@@ -1,11 +1,7 @@
 package swip.ch02locatingelements;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-
-import java.util.List;
-import java.util.function.BiPredicate;
 
 public class SimpleTable extends DelegatingWebElement implements Table {
 
@@ -22,34 +18,9 @@ public class SimpleTable extends DelegatingWebElement implements Table {
 
     @Override
     public WebElement getBodyCell(int rowNumber, String header) {
-        ColumnNumberFinder finder = new ColumnNumberFinder(header); // # create an object to find the column
-        int columnNumber = finder.find(delegate.findElement(By.tagName("thead"))); // # find the column
+        ColumnNumberFinder finder = new ColumnNumberFinder(header);
+        int columnNumber = finder.find(delegate.findElement(By.tagName("thead")));
         return getBodyCell(rowNumber, columnNumber);
-    }
-
-    @Override
-    public WebElement getBodyCell(BiPredicate<WebElement, CellLocation> cellMatcher) {
-
-        List<WebElement> bodyRows = delegate.findElements(By.cssSelector("tbody tr"));
-
-        for (int rowIndex = 0; rowIndex < bodyRows.size(); rowIndex++) { // # perform an indexed iteration over the rows
-
-            WebElement bodyRow = bodyRows.get(rowIndex);
-
-            List<WebElement> cells = bodyRow.findElements(By.tagName("td"));
-
-            for (int columnIndex = 0; columnIndex < cells.size(); columnIndex++) { // # perform an indexed iteration over the cells
-
-                WebElement cell = cells.get(columnIndex);
-                CellLocation cellLocation = new CellLocation(rowIndex + 1, columnIndex + 1);
-
-                if (cellMatcher.test(cell, cellLocation)) { // # if the cell matches, then return it
-                    return cell;
-                }
-            }
-        }
-
-        throw new NoSuchElementException("cannot find cell");
     }
 
     @Override
--- src/test/java/swip/ch02locatingelements/Table.java
+++ src/test/java/swip/ch02locatingelements/Table.java
@@ -2,15 +2,11 @@ package swip.ch02locatingelements;
 
 import org.openqa.selenium.WebElement;
 
-import java.util.function.BiPredicate;
-
 public interface Table extends WebElement {
 
     WebElement getBodyCell(int rowNumber, int columnNumber);
 
     WebElement getBodyCell(int rowNumber, String header);
-
-    WebElement getBodyCell(BiPredicate<WebElement, CellLocation> cellMatcher);
 
     int getWidth();
 
