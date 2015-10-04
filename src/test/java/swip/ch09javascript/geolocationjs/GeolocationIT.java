200553a450316cb2e9048cdf1dcac5d9d9b56831
alex.collins <alex.e.c@gmail.com>
2015-10-04 16:42:02 +0100

    ch09: updated examples

--- src/test/java/swip/ch09javascript/geolocationjs/GeolocationIT.java
+++ src/test/java/swip/ch09javascript/geolocationjs/GeolocationIT.java
@@ -17,19 +17,19 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(WebDriverRunner.class)
 @Config(exclude = "browserName=htmlunit")
-public class GeolocationIT<W extends WebDriver & JavascriptExecutor> {
+public class GeolocationIT {
     @Inject
-    private W driver;
+    private WebDriver driver;
 
     @Before
     public void setUp() throws Exception {
-        driver.get("http://localhost:8080/geolocation.html");
+        driver.get("/geolocation.html");
     }
 
     @Test
     public void geolocationInjection() throws Exception {
 
-        driver.executeScript(
+        ((JavascriptExecutor) driver).executeScript(
                 String.format(
                         "navigator.geolocation = navigator.geolocation || {};" +
                                 "navigator.geolocation.getCurrentPosition = function(ok,err){" +
@@ -41,20 +41,20 @@ public class GeolocationIT<W extends WebDriver & JavascriptExecutor> {
 
         driver.findElement(By.id("locate")).click();
 
-        WebElement statusElement = driver.findElement(By.id("status"));
+        WebElement location = driver.findElement(By.id("location"));
 
-        new WebDriverWait(driver, 10).until((WebDriver d) -> !statusElement.getText().equals("Loading..."));
+        new WebDriverWait(driver, 10).until((WebDriver d) -> !location.getText().equals("Loading..."));
 
-        assertEquals("You state you are at + 51.5106766, -0.1231314", statusElement.getText());
+        assertEquals("You state you are at + 51.5106766, -0.1231314", location.getText());
     }
 
     @Test
     public void positionError() throws Exception {
 
-        driver.executeScript(
+        ((JavascriptExecutor) driver).executeScript(
                 String.format(
                         "navigator.geolocation = navigator.geolocation || {};" +
-                        "navigator.geolocation.getCurrentPosition = function(ok,err){" +
+                                "navigator.geolocation.getCurrentPosition = function(ok,err){" +
                                 "err({'error': {" +
                                 "'PERMISSION_DENIED': 1, " +
                                 "'POSITION_UNAVAILABLE': 2, " +
@@ -66,10 +66,10 @@ public class GeolocationIT<W extends WebDriver & JavascriptExecutor> {
 
         driver.findElement(By.id("locate")).click();
 
-        WebElement statusElement = driver.findElement(By.id("status"));
+        WebElement location = driver.findElement(By.id("location"));
 
-        new WebDriverWait(driver, 10).until((WebDriver d) -> !statusElement.getText().equals("Loading..."));
+        new WebDriverWait(driver, 10).until((WebDriver d) -> !location.getText().equals("Loading..."));
 
-        assertEquals("Error: 1 User denied Geolocation", statusElement.getText());
+        assertEquals("Error: 1 User denied Geolocation", location.getText());
     }
 }
