6c9332458c7d65a1bbf351dbf2d68551ad971ff1
alex.collins <alex.e.c@gmail.com>
2015-10-05 15:47:41 +0100

    updated base url decorator

--- src/test/java/swip/ch07managingwebdriver/ConfigFactory.java
+++ src/test/java/swip/ch07managingwebdriver/ConfigFactory.java
@@ -1,17 +1,6 @@
 package swip.ch07managingwebdriver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-
 public class ConfigFactory {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigFactory.class);
-    public static final String BASE_URL = baseUrl(LOGGER);
-
-    private static String baseUrl(Logger logger) {
-        String baseUrl = System.getProperty("webdriver.baseUrl", "http://localhost:8080");
-        logger.info("baseUrl " + baseUrl);
-        return baseUrl;
-    }
+    public static final String BASE_URL =
+            System.getProperty("webdriver.baseUrl", "http://localhost:8080");
 }
--- src/test/java/swip/ch11decorating/baseurl/BaseUrlDecorator.java
+++ src/test/java/swip/ch11decorating/baseurl/BaseUrlDecorator.java
@@ -41,5 +41,4 @@ public class BaseUrlDecorator {
                 interfaces,
                 invocationHandler);
     }
-
 }
