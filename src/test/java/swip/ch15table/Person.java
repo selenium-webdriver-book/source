b55010b6bf96ced33e0ea101f19e96f792f0b4c0
alex.collins <alex.e.c@gmail.com>
2015-11-22 13:28:14 +0000

    ch15: made field final

--- src/test/java/swip/ch15table/Person.java
+++ src/test/java/swip/ch15table/Person.java
@@ -7,7 +7,7 @@ public class Person {
     private final String firstName;
     private final String lastName;
     private final int points;
-    private int number;
+    private final int number;
 
     public Person(int number, String firstName, String lastName, int points) {
         this.number = number;
