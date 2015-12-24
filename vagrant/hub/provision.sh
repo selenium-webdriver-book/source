8bcad40791331467d1bd8f9ec745f3b03e40b643
alex.collins <alex.e.c@gmail.com>
2015-12-24 00:51:37 +0000

    vagrant

--- vagrant/hub/provision.sh
+++ vagrant/hub/provision.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
 set -eux
+
 apt-get update
 apt-get install -y openjdk-7-jre
-wget -c http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar
+wget -c http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
 
 cat > /etc/init/selenium-hub.conf <<EOF
 # selenium-hub - Run A Selenium Hub.
@@ -11,8 +13,8 @@ start on runlevel [2345]
 stop on runlevel [016]
 respawn
 
-exec java -jar /home/vagrant/selenium-server-standalone-2.46.0.jar -role hub
+exec java -jar /home/vagrant/selenium-server-standalone-2.48.2.jar -role hub
 EOF
 
 initctl reload-configuration
-service selenium-hub start 
+service selenium-hub start
