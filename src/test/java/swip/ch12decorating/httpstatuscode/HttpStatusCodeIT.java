fe105d5ffbeaccf13556d61c23319b7cc055d80f
alex.collins <alex.collins@gamesys.co.uk>
2016-01-09 21:53:15 +0000

    changed to use WebDriverRunner

--- src/test/java/swip/ch12decorating/httpstatuscode/HttpStatusCodeIT.java
+++ src/test/java/swip/ch12decorating/httpstatuscode/HttpStatusCodeIT.java
@@ -3,17 +3,14 @@ package swip.ch12decorating.httpstatuscode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import swip.framework.HttpStatusCodeSupplier;
-import swip.framework.WebDriverConfig;
+import swip.framework.WebDriverRunner;
 
 import javax.inject.Inject;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = WebDriverConfig.class)
+@RunWith(WebDriverRunner.class)
 public class HttpStatusCodeIT {
 
     @Inject private WebDriver driver;
