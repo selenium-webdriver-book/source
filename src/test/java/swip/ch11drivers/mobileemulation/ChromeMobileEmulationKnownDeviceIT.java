47539ed167504df7e05a0abacc2c61acf5197f4f
alex.collins <alex.collins@gamesys.co.uk>
2016-01-17 11:43:07 +0000

    renamed ChromeMobileEmulationKnowDeviceIT -> ChromeMobileEmulationKnownDeviceIT

--- src/test/java/swip/ch11drivers/mobileemulation/ChromeMobileEmulationKnownDeviceIT.java
+++ src/test/java/swip/ch11drivers/mobileemulation/ChromeMobileEmulationKnownDeviceIT.java
@@ -13,7 +13,7 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ChromeMobileEmulationKnowDeviceIT {
+public class ChromeMobileEmulationKnownDeviceIT {
 
     private WebDriver driver;
 
