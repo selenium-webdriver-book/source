cdf653661b8617458ca675029fb3b9c659acd9de
alex.e.c@gmail.com <Christine3>
2016-01-30 17:30:14 +0000

    Added order confirmation page

--- src/main/java/swip/bookstore/app/CheckoutController.java
+++ src/main/java/swip/bookstore/app/CheckoutController.java
@@ -1,6 +1,7 @@
 package swip.bookstore.app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +13,13 @@ import javax.validation.Valid;
 public class CheckoutController {
 
     @RequestMapping(value = "/checkout", method = RequestMethod.POST)
-    public String addToCart(@Valid CheckoutRequest checkoutRequest, BindingResult result) {
+    public String addToCart(@Valid CheckoutRequest checkoutRequest, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
             return "cart";
         }
+
+        model.addAttribute("orderNumber", String.format("%08d", System.currentTimeMillis() / 1000000));
 
         return "checkout";
     }
---
+++ src/main/webapp/WEB-INF/views/checkout.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <jsp:include page="head.jsp"/>
+    <title>BookStore</title>
+</head>
+<body>
+<jsp:include page="nav.jsp"/>
+
+<div class="container">
+    <h1>Thank you for your order</h1>
+    <p>
+        Order number #${orderNumber}.
+    </p>
+
+</div>
+</body>
