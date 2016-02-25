cbd6812a8d8ded75cc69789609b418dcd4c93959
Alex Collins <alex.e.c@gmail.com>
2016-02-25 22:08:47 +0000

    Implemented all interfaces RemoteWebDriver implemented.

--- src/test/java/swip/framework/DelegatingWebDriver.java
+++ src/test/java/swip/framework/DelegatingWebDriver.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.HasInputDevices;
 import org.openqa.selenium.interactions.Keyboard;
 import org.openqa.selenium.interactions.Mouse;
+import org.openqa.selenium.internal.*;
 
 import java.util.List;
 import java.util.Set;
 
-class DelegatingWebDriver implements WebDriver, JavascriptExecutor, TakesScreenshot, HasInputDevices {
+/**
+ * Implements all the same interfaces as {@link org.openqa.selenium.remote.RemoteWebDriver}.
+ *
+ * @see org.openqa.selenium.remote.RemoteWebDriver
+ */
+class DelegatingWebDriver implements WebDriver, JavascriptExecutor, FindsById, FindsByClassName, FindsByLinkText, FindsByName, FindsByCssSelector, FindsByTagName, FindsByXPath, HasInputDevices, HasCapabilities, TakesScreenshot {
     private final WebDriver driver;
 
     DelegatingWebDriver(WebDriver driver) {
@@ -103,5 +109,90 @@ class DelegatingWebDriver implements WebDriver, JavascriptExecutor, TakesScreens
     @Override
     public Mouse getMouse() {
         return ((HasInputDevices) driver).getMouse();
+    }
+
+    @Override
+    public WebElement findElementByClassName(String using) {
+        return ((FindsByClassName) driver).findElementByClassName(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByClassName(String using) {
+        return ((FindsByClassName) driver).findElementsByClassName(using);
+    }
+
+    @Override
+    public WebElement findElementByCssSelector(String using) {
+        return ((FindsByCssSelector) driver).findElementByCssSelector(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByCssSelector(String using) {
+        return ((FindsByCssSelector) driver).findElementsByCssSelector(using);
+    }
+
+    @Override
+    public WebElement findElementById(String using) {
+        return ((FindsById) driver).findElementById(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsById(String using) {
+        return ((FindsById) driver).findElementsById(using);
+    }
+
+    @Override
+    public WebElement findElementByLinkText(String using) {
+        return ((FindsByLinkText) driver).findElementByLinkText(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByLinkText(String using) {
+        return ((FindsByLinkText) driver).findElementsByLinkText(using);
+    }
+
+    @Override
+    public WebElement findElementByPartialLinkText(String using) {
+        return ((FindsByLinkText) driver).findElementByPartialLinkText(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByPartialLinkText(String using) {
+        return ((FindsByLinkText) driver).findElementsByPartialLinkText(using);
+    }
+
+    @Override
+    public WebElement findElementByName(String using) {
+        return ((FindsByName) driver).findElementByName(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByName(String using) {
+        return ((FindsByName) driver).findElementsByName(using);
+    }
+
+    @Override
+    public WebElement findElementByTagName(String using) {
+        return ((FindsByTagName) driver).findElementByTagName(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByTagName(String using) {
+        return ((FindsByTagName) driver).findElementsByTagName(using);
+    }
+
+    @Override
+    public WebElement findElementByXPath(String using) {
+        return ((FindsByXPath) driver).findElementByXPath(using);
+    }
+
+    @Override
+    public List<WebElement> findElementsByXPath(String using) {
+        return ((FindsByXPath) driver).findElementsByXPath(using);
+    }
+
+    @Override
+    public Capabilities getCapabilities() {
+        return ((HasCapabilities) driver).getCapabilities();
     }
 }
