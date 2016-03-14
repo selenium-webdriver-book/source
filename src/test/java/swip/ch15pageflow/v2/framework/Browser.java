f35b35ee54c10d4b1b86b8bff342af423c0e8d80
Yujun Liang <yujunliang@hotmail.com>
2016-03-13 23:08:53 -0500

    Refactoring.

--- src/test/java/swip/ch15pageflow/v2/framework/Browser.java
+++ src/test/java/swip/ch15pageflow/v2/framework/Browser.java
@@ -9,13 +9,69 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-public class Browser extends DelegatingWebDriver implements ExplicitWait, SearchScope {
+public class Browser extends DelegatingSearchContext<WebDriver> implements WebDriver {
 
     public Browser(WebDriver driver) {
         super(driver);
+    }
+
+    @Override
+    public void get(String url) {
+        delegate.get(url);
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        return delegate.getCurrentUrl();
+    }
+
+    @Override
+    public String getTitle() {
+        return delegate.getTitle();
+    }
+
+    @Override
+    public String getPageSource() {
+        return delegate.getPageSource();
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    @Override
+    public void quit() {
+        delegate.quit();
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        return delegate.getWindowHandles();
+    }
+
+    @Override
+    public String getWindowHandle() {
+        return delegate.getWindowHandle();
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        return delegate.switchTo();
+    }
+
+    @Override
+    public Navigation navigate() {
+        return delegate.navigate();
+    }
+
+    @Override
+    public Options manage() {
+        return delegate.manage();
     }
 
     public void setInputText(Supplier<By> by, Object value) {
