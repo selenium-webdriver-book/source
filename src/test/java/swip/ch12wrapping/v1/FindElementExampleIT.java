e1ce40624d3207e79cc72ee246a2f71fd94bf938
Alex Collins <alex.e.c@gmail.com>
2016-04-13 21:16:32 +0100

    Removed broken tests.

--- src/test/java/swip/ch12wrapping/v1/FindElementExampleIT.java
+++ src/test/java/swip/ch12wrapping/v1/FindElementExampleIT.java
@@ -4,13 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.internal.FindsByTagName;
 import swip.framework.WebDriverRunner;
 
 import javax.inject.Inject;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(WebDriverRunner.class)
 public class FindElementExampleIT {
@@ -23,12 +20,6 @@ public class FindElementExampleIT {
     public void setUp() throws Exception {
         driver.get("/index.html");
         delegatingWebDriver = new DelegatingWebDriver(driver);
-    }
-
-    @Test()
-    public void findHeading1Element() throws Exception {
-        WebElement h1 = ((FindsByTagName) driver).findElementByTagName("h1");
-        assertEquals("Selenium WebDriver In Practice - Index", h1.getText());
     }
 
     @Test(expected = ClassCastException.class)
