dee2c47d1f7690d22e811e904a0d19e2284b85ec
Alex Collins <alex.e.c@gmail.com>
2016-04-16 11:21:36 +0100

    Added an example of the error you would get if the modal has popped up.

--- src/test/java/swip/ch08windows/modals/SyntheticModalIT.java
+++ src/test/java/swip/ch08windows/modals/SyntheticModalIT.java
@@ -2,16 +2,17 @@ package swip.ch08windows.modals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import swip.framework.WebDriverRunner;
 
 import javax.inject.Inject;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 @RunWith(WebDriverRunner.class)
 public class SyntheticModalIT {
@@ -31,5 +32,25 @@ public class SyntheticModalIT {
 
         assertFalse(driver.findElement(By.className("modal-dialog")).isDisplayed());
 
+    }
+
+    @Test
+    public void demonstrateAccessToUnclickableElement() throws Exception {
+
+        driver.get("/popups.html");
+
+        driver.findElement(By.linkText("Modal")).click();
+
+        new WebDriverWait(driver, 2).until(Modals.modalIsDisplayed());
+
+        WebElement openModalButton = driver.findElement(By.linkText("Modal"));
+
+        try {
+            openModalButton.click();
+            fail("should not be able to click when modal is displayed");
+        } catch(WebDriverException e) {
+            assertThat(e.getMessage(),
+                    containsString("Other element would receive the click"));
+        }
     }
 }
