738736143b9640f6272ad01846cd04b9489ca7fc
Alex Collins <alex.e.c@gmail.com>
2016-10-23 19:45:24 +0100

    fixed for Chrome

--- src/test/java/swb/ch01intro/HelloWebDriverIT.java
+++ src/test/java/swb/ch01intro/HelloWebDriverIT.java
@@ -6,32 +6,32 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.Assert.assertEquals;
 
-public class HelloWebDriverIT { // we use the IT suffix for test in this book, this is the Maven convention for integration tests
+public class HelloWebDriverIT {
 
     private WebDriver driver;
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "target/geckodriver");
-        driver = new FirefoxDriver(); // create a new driver which connected to an instance of the Firefox browser
+        System.setProperty("webdriver.chrome.driver", "target/chromedriver");
+        driver = new ChromeDriver();
     }
 
     @After
     public void tearDown() throws Exception {
-        driver.quit(); // instruct the browser to quit
+        driver.quit();
     }
 
     @Test
     public void helloWebDriver() throws Exception {
 
-        driver.get("http://localhost:8080/hello-webdriver.html"); // open a web page in the browser
+        driver.get("http://localhost:8080/hello-webdriver.html");
 
-        WebElement pageHeading = driver.findElement(By.tagName("h1")); // locate an element on the current page, in this case the page's heading
+        WebElement pageHeading = driver.findElement(By.tagName("h1"));
 
-        assertEquals("Hello WebDriver!", pageHeading.getText()); // verify that the heading is the value you expect
+        assertEquals("Hello WebDriver!", pageHeading.getText());
     }
 }
